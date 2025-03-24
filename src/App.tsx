73f1e0581fed1d44627b852d814b3d7f7e1e33fe
studioalign <amyosborneseo@gmail.com>
2025-03-24 14:52:09 +0000

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignupForm from "./components/SignupForm";
 import SigninForm from "./components/SigninForm";
